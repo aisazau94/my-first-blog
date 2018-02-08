1ace7ea8f7255f85593c4be27043bbc71794b205
Alejandra <aisazau@gmail.com>
2018-02-08 16:27:49 -0500

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 class Post(models.Model):
 	author = models.ForeignKey('auth.User') #este es un vinculo con otro modelo.
-	title = models.CharField(max_length=200) # defines un texto con un número limitado de caracteres.
+	title = models.CharField(max_length=200) # defines un texto con un numero limitado de caracteres.
 	text = models.TextField() #esto es para textos largos sin un limite
 	created_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True, null=True)
